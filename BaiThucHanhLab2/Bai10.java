f1c8ddd6132f1491c96b2f097f42853a9697d82a
VuQuyen0612 <quyenvuhd2000@gmail.com>
2023-03-21 07:13:54 +0700

    first commit

---
+++ BaiThucHanhLab2/Bai10.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class bai10 {
+    public static void main(String[] args) {
+        String a;
+        Scanner sc = new Scanner(System.in);        
+        do {        
+            System.out.println("nhap chuoi: ");
+            a = sc.nextLine();
+        } while (a.length()>80);       
+    }
+}
